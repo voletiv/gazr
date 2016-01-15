1c45876e94b1eef839b4b571940343c4ef4a1eed
Séverin Lemaignan <severin.lemaignan@plymouth.ac.uk>
2016-01-15 17:33:43 +0000

    Display eyes origins

--- src/head_pose_estimation.cpp
+++ src/head_pose_estimation.cpp
@@ -316,21 +316,56 @@ head_pose HeadPoseEstimation::pose(size_t face_idx) const
 //        circle(_debug, point,2, Scalar(0,255,255),2);
 //    }
 //
-      std::vector<Point3d> axes;
-      std::vector<Point2d> projected_axes;
+    Matx44f left_eye_pose = {
+        1,    0,    0,    -12,
+        0,    1,    0,    35,
+        0,    0,    1,    0,
+        0,    0,    0,    1
+    };
+    Matx44f right_eye_pose = {
+        1,    0,    0,    -12,
+        0,    1,    0,    -35,
+        0,    0,    1,    0,
+        0,    0,    0,    1
+    };
+
+
+    std::vector<Point3f> axes;
+    axes.push_back(toPoint3f(left_eye_pose * Vec4f(0,0,0,1)));
+    axes.push_back(toPoint3f(left_eye_pose * Vec4f(50,0,0,1)));
+    axes.push_back(toPoint3f(left_eye_pose * Vec4f(0,50,0,1)));
+    axes.push_back(toPoint3f(left_eye_pose * Vec4f(0,0,50,1)));
+
+    std::vector<Point2f> projected_axes;
+
+    projectPoints(axes, rvec, tvec, projection, noArray(), projected_axes);
+
+    line(_debug, projected_axes[0], projected_axes[3], Scalar(255,0,0),2,CV_AA);
+    line(_debug, projected_axes[0], projected_axes[2], Scalar(0,255,0),2,CV_AA);
+    line(_debug, projected_axes[0], projected_axes[1], Scalar(0,0,255),2,CV_AA);
 
     axes.clear();
-    axes.push_back(toPoint3d(pose * Vec4d(0,0,0,1)));
-    axes.push_back(toPoint3d(pose * Vec4d(0.05,0,0,1))); // axis are 5cm long
-    axes.push_back(toPoint3d(pose * Vec4d(0,0.05,0,1)));
-    axes.push_back(toPoint3d(pose * Vec4d(0,0,0.05,1)));
+    axes.push_back(toPoint3f(right_eye_pose * Vec4f(0,0,0,1)));
+    axes.push_back(toPoint3f(right_eye_pose * Vec4f(50,0,0,1)));
+    axes.push_back(toPoint3f(right_eye_pose * Vec4f(0,50,0,1)));
+    axes.push_back(toPoint3f(right_eye_pose * Vec4f(0,0,50,1)));
 
-    projectPoints(axes, Vec3f(0.,0.,0.), Vec3f(0.,0.,0.), projection, noArray(), projected_axes);
 
-    line(_debug, projected_axes[0], projected_axes[1], Scalar(255,0,0),2,CV_AA);
+    projectPoints(axes, rvec, tvec, projection, noArray(), projected_axes);
+
+    line(_debug, projected_axes[0], projected_axes[3], Scalar(255,0,0),2,CV_AA);
     line(_debug, projected_axes[0], projected_axes[2], Scalar(0,255,0),2,CV_AA);
-    line(_debug, projected_axes[0], projected_axes[3], Scalar(0,0,255),2,CV_AA);
+    line(_debug, projected_axes[0], projected_axes[1], Scalar(0,0,255),2,CV_AA);
+    
 
+
+    axes.clear();
+    axes.push_back(toPoint3f(left_eye_pose * Vec4f(0,0,0,1)));
+    axes.push_back(toPoint3f(left_eye_pose * Vec4f(1000,0,0,1)));
+
+    projectPoints(axes, rvec, tvec, projection, noArray(), projected_axes);
+
+    line(_debug, projected_axes[0], projected_axes[1], Scalar(255,255,0),1,CV_AA);
 
 
 
